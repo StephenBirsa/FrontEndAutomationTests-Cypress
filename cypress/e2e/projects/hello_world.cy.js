ff66922dd2bf21681acf21e311b231bd6541fdc8
Stephen <27788517+StephenBirsa@users.noreply.github.com>
2023-11-03 17:29:33 +1100

    cleaned up test

--- cypress/e2e/projects/hello_world.cy.js
+++ cypress/e2e/projects/hello_world.cy.js
@@ -1,12 +1,9 @@
 /// <reference types="cypress" />
 
-context('Window', () => {
-  describe('Hello World Testing Webpage', () => {
-    it('Type Hello World in Google Page', () => {
-      cy.visit('https://www.google.com.au/')
-      cy.title().should('eq', 'Google')
-      cy.get('[title="Search"]').type("Hello, World!");
-      cy.get('[jsmodel="hspDDf"]').click();
-    })
-  });
+describe('Hello World Test', () => {
+  it('Type Hello World in Google Page', () => {
+    cy.visit('https://www.google.com.au/')
+    cy.title().should('eq', 'Google')
+    cy.get('[title="Search"]').type("Hello, World!").blur();
+  })
 });
